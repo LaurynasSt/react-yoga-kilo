75c03413362ca6cd6dca11454f2f38f190379f19
orezdrah <l.stackunas@gmail.com>
2021-08-26 18:59:24 +0300

    created heading title & sections title component with styles

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import './App.css';
 import Header from './components/Header';
+import SectionTitle from './components/SectionTitle';
 
 function App() {
   return (
     <div className="App">
       <Header></Header>
-    </div>
+      <main>
+        <section className="heading-container">
+          <p className="plans-ordered">Over <span className="plans-ordered-count">52 147</span> plans ordered.</p>
+          <h1 className="heading-title">Get access to your yoga program now!</h1>
+        </section>
+        <section className="plans-program-container">
+          <SectionTitle>Choose your plan and get <span className="free-trial-orange-text">7 days free trial</span></SectionTitle>
+        </section>
+      </main>
+    </div >
   );
 }
 
---
+++ src/components/SectionTitle/index.js
@@ -0,0 +1,9 @@
+import "./index.css";
+
+function SectionTitle({ children }) {
+    return (
+        <h3>{children}</h3>
+    );
+}
+
+export default SectionTitle;
